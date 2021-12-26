b60cef46624ba3bb4ab31c614aef151aaea31fd2
prameshmca5 <rameshkp2@gmail.com>
2021-12-26 20:09:37 +0800

    Added Custom Query with Junit Testing.

--- src/main/java/com/ramesh/microservice/repository/EmployeeRepository.java
+++ src/main/java/com/ramesh/microservice/repository/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.ramesh.microservice.repository;
 
 import com.ramesh.microservice.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +12,20 @@ import java.util.Optional;
 public interface EmployeeRepository  extends JpaRepository<Employee, Long> {
 
     Optional<Employee> findByEmail(String Email);
+
+    // Direct Query annotations
+    @Query("select e from  Employee  e where e.firstName= ?1 and e.lastName= ?2")
+    Employee findJpql(String firstName, String lastName);
+
+    // Pass value to Named Params
+    @Query("Select e from Employee e where e.firstName=:firstName and e.lastName=:lastName")
+    Employee findJpqlParams(@Param("firstName") String firstName, @Param("lastName") String lastName);
+
+    // Write Query from Native into JPQL to Native Querys
+    @Query(value = "select * from employess e where e.first_name=?1 and e.last_name=?2", nativeQuery = true)
+    Employee findQueryJpqlNative(String firstName, String lastName);
+
+    // Write Query from Native into JPQL to Native Querys with Params
+    @Query(value = "select * from employess e where e.first_name=:firstName and e.last_name=:lastName", nativeQuery = true)
+    Employee findQueryJpqlNativeWithParams(@Param("firstName") String firstName, @Param("lastName") String lastName);
 }
--- src/test/java/com/ramesh/microservice/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/ramesh/microservice/repository/EmployeeRepositoryTest.java
@@ -2,6 +2,8 @@ package com.ramesh.microservice.repository;
 
 import com.ramesh.microservice.model.Employee;
 import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +19,17 @@ public class EmployeeRepositoryTest {
     @Autowired
     public EmployeeRepository employeeRepository;
 
+    private Employee employee;
+
+    @BeforeEach
+    public void setup(){
+        employee = Employee.builder()
+                .firstName("Ramesh")
+                .lastName("Pongiannan")
+                .email("ramesh@gmail.com")
+                .build();
+    }
+
     // BDD Model Test -> Behavioral Data Driven
     // Given - When - Then
 
@@ -24,12 +37,12 @@ public class EmployeeRepositoryTest {
     @Test
     public void givenEmployeeObj_whenSave_thenReturnSavedEmployee() {
         // Given - Setup
-        Employee employee = Employee.builder()
+      /*  Employee employee = Employee.builder()
                 .firstName("Ramesh")
                 .lastName("Pongiannan")
                 .email("ramesh@gmail.com")
                 .build();
-
+        */
         // When - action
         Employee saveEmployee = employeeRepository.save(employee);
         // Then - verify the output
@@ -43,18 +56,18 @@ public class EmployeeRepositoryTest {
    public void givenEmployeeList_whenFindAll_thenEmployeeList(){
 
        // Given - Setup
-       Employee employee1 = Employee.builder()
+      /*   Employee employee1 = Employee.builder()
                .firstName("Ramesh")
                .lastName("Pongiannan")
                .email("ramesh@gmail.com")
                .build();
-
+*/
        Employee employee2 = Employee.builder()
                .firstName("Ramesh2")
                .lastName("Pongiannan2")
                .email("ramesh2@gmail.com")
                .build();
-       employeeRepository.save(employee1);
+       employeeRepository.save(employee);
        employeeRepository.save(employee2);
 
        // When - Action
@@ -71,14 +84,16 @@ public class EmployeeRepositoryTest {
   public void givenEmployee_whenFindbyId_thenEmloyeeDetails(){
 
       // Given - Setup
-      Employee employee3 = Employee.builder()
+    /*    Employee employee3 = Employee.builder()
               .firstName("Ramesh")
               .lastName("Pongiannan")
               .email("ramesh@gmail.com")
               .build();
-      employeeRepository.save(employee3);
+
+     */
+      employeeRepository.save(employee);
       // When - Action
-      Optional<Employee> emp = employeeRepository.findById(employee3.getId());
+      Optional<Employee> emp = employeeRepository.findById(employee.getId());
       // Then - Verify or Output
       assertThat(emp.get().getId()).isNotNull();
       assertThat(emp.get().getId()).isEqualTo(1);
@@ -90,16 +105,133 @@ public class EmployeeRepositoryTest {
   public void givenEmployee_whenFindbyEmail_thenEmployeeDetails() {
 
       // Given - Setup
-      Employee employee4 = Employee.builder()
+     /*   Employee employee4 = Employee.builder()
               .firstName("Ramesh")
               .lastName("Pongiannan")
               .email("ramesh44@gmail.com")
               .build();
-      employeeRepository.save(employee4);
+
+      */
+      employeeRepository.save(employee);
       // When - Action
-      Optional<Employee> emp = employeeRepository.findByEmail(employee4.getEmail());
+      Optional<Employee> emp = employeeRepository.findByEmail(employee.getEmail());
       // Then - Verify or Output
       assertThat(emp.get().getEmail()).isNotNull();
-      assertThat(emp.get().getEmail()).isEqualTo("ramesh44@gmail.com");
+      assertThat(emp.get().getEmail()).isEqualTo("ramesh@gmail.com");
   }
+
+     // Junit for Update Employee details
+    @DisplayName("UpdateEmployeeDetails")
+     @Test
+     public void givenEmployee_whenUpdateEmployee_thenGetEmployeeDetails(){
+
+         // Given - Setup
+     /*     Employee employee4 = Employee.builder()
+                .firstName("Ramesh")
+                .lastName("Pongiannan")
+                .email("ramesh44@gmail.com")
+                .build();
+
+      */
+        employeeRepository.save(employee);
+         // When - Action
+        Optional<Employee> emp = employeeRepository.findByEmail(employee.getEmail());
+        emp.get().setEmail("Laxsha@gmail.com");
+        employeeRepository.save(emp.get());
+         // Then - Verify or Output
+        assertThat(emp.get().getEmail()).isNotNull();
+        assertThat(emp.get().getEmail()).isEqualTo("Laxsha@gmail.com");
+     }
+
+    // Junit for Update Employee details
+    @DisplayName("DeleteEmployeeDetails")
+    @Test
+    public void givenEmployee_whenDeleteEmployee_thenGetEmployeeDetails(){
+
+        // Given - Setup
+       /*   Employee employee4 = Employee.builder()
+                .firstName("Ramesh")
+                .lastName("Pongiannan")
+                .email("ramesh44@gmail.com")
+                .build();
+
+        */
+        employeeRepository.save(employee);
+        // When - Action
+        Optional<Employee> emp = employeeRepository.findById(employee.getId());
+       // emp.get().setEmail("Laxsha@gmail.com");
+        employeeRepository.delete(emp.get());
+        // Then - Verify or Output
+        assertThat(emp).isNotNull();
+       assertThat(emp.get().getId()).isEqualTo(6);
+    }
+
+    // Junit for Get Custom Query
+    @DisplayName("EmployeeDetailsGetCustomQueryforJPQL")
+    @Test
+    public void givenEmployee_whenGetCustomJPQL_thenGetJpqlResult(){
+
+       // Given - Setup
+
+       // When - Action
+        employeeRepository.save(employee);
+        employee.setFirstName("Ramesh");
+        employee.setLastName("Pongiannan");
+        employee = employeeRepository.findJpql(employee.getFirstName(), employee.getLastName());
+       // Then - Verify or Output
+        assertThat(employee).isNotNull();
+        assertThat(employee.getId()).isEqualTo(1);
+    }
+    // Junit for Get Custom Query
+    @DisplayName("EmployeeDetailsGetCustomQueryforJPQLNameParams")
+    @Test
+    public void givenEmployee_whenGetCustomJPQLNamedParams_thenGetJpqlResult(){
+
+        // Given - Setup
+
+        // When - Action
+        employeeRepository.save(employee);
+        employee.setFirstName("Ramesh");
+        employee.setLastName("Pongiannan");
+        employee = employeeRepository.findJpqlParams(employee.getFirstName(), employee.getLastName());
+        // Then - Verify or Output
+        assertThat(employee).isNotNull();
+        assertThat(employee.getId()).isEqualTo(1);
+    }
+
+    // Junit for Get Custom Query
+    @DisplayName("EmployeeDetailsGetCustomQueryforJPQLNative")
+    @Test
+    public void givenEmployee_whenGetCustomJPQLNative_thenGetJpqlResult(){
+
+        // Given - Setup
+
+        // When - Action
+        employeeRepository.save(employee);
+       // employee.setFirstName("Ramesh");
+       // employee.setLastName("Pongiannan");
+        employee = employeeRepository.findQueryJpqlNative(employee.getFirstName(), employee.getLastName());
+       // employee = employeeRepository.findQueryJpqlNative("Ramesh", "Pongiannan");
+        // Then - Verify or Output
+        assertThat(employee).isNotNull();
+        assertThat(employee.getId()).isEqualTo(1);
+    }
+
+    // Junit for Get Custom Query
+    @DisplayName("EmployeeDetailsGetCustomQueryforJPQLNativeWithParams")
+    @Test
+    public void givenEmployee_whenGetCustomJPQLNativeWithParams_thenGetJpqlResult(){
+
+    // Given - Setup
+
+    // When - Action
+    employeeRepository.save(employee);
+    // employee.setFirstName("Ramesh");
+    // employee.setLastName("Pongiannan");
+    employee = employeeRepository.findQueryJpqlNativeWithParams(employee.getFirstName(), employee.getLastName());
+    // employee = employeeRepository.findQueryJpqlNative("Ramesh", "Pongiannan");
+    // Then - Verify or Output
+    assertThat(employee).isNotNull();
+    assertThat(employee.getId()).isEqualTo(1);
+}
 }
